2d97428e5354856317734b3c3266ef4d88b32679
Ryan Thompson <rthomp2113@gmail.com>
2023-01-25 09:56:06 -0500

    feat: added users api call

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState();
 
-  
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      const userData = response.data;
+      setUsers(userData);
+    })
+  }, [])
+
   return (
     <div className="App">
-     
-       
-   
-      
+      <h1>User Accounts</h1>
+      {!users && 'loading'}
+      {users && users.map(user => {
+        const { id, name, username, email, address : {city}} = user;
+        const userDataString = `id - ${id} | username - ${username} | name - ${name} | email - ${email} | city - ${city}`
+        return <h6 key={id}>{userDataString}</h6>
+      })} 
     </div>
   );
 }
